5c0a24563f081a27d61acf141d60bdac5c262a94
zhousujuan <179997445@qq.com>
2023-12-01 18:44:47 +0800

    导航

--- src/components/layouts/BaseSide.vue
+++ src/components/layouts/BaseSide.vue
@@ -1,41 +1,58 @@
 <template>
   <el-menu
-    default-active="2"
+    active-text-color="#ffd04b"
+    background-color="#172032"
     class="el-menu-vertical-demo"
-    :collapse="isCollapse"
+    default-active="2"
+    text-color="#fff"
     @open="handleOpen"
     @close="handleClose"
   >
     <el-sub-menu index="1">
       <template #title>
         <el-icon><location /></el-icon>
-        <span>Navigator One</span>
+        <span>应用</span>
       </template>
       <el-menu-item-group>
-        <template #title><span>Group One</span></template>
-        <el-menu-item index="1-1">item one</el-menu-item>
-        <el-menu-item index="1-2">item two</el-menu-item>
+        <el-menu-item index="1-1">应用列表</el-menu-item>
       </el-menu-item-group>
-      <el-menu-item-group title="Group Two">
-        <el-menu-item index="1-3">item three</el-menu-item>
-      </el-menu-item-group>
-      <el-sub-menu index="1-4">
-        <template #title><span>item four</span></template>
-        <el-menu-item index="1-4-1">item one</el-menu-item>
-      </el-sub-menu>
     </el-sub-menu>
-    <el-menu-item index="2">
-      <el-icon><icon-menu /></el-icon>
-      <template #title>Navigator Two</template>
-    </el-menu-item>
-    <el-menu-item index="3" disabled>
-      <el-icon><document /></el-icon>
-      <template #title>Navigator Three</template>
-    </el-menu-item>
-    <el-menu-item index="4">
-      <el-icon><setting /></el-icon>
-      <template #title>Navigator Four</template>
-    </el-menu-item>
+    <el-sub-menu index="2">
+      <template #title>
+        <el-icon><location /></el-icon>
+        <span>发布</span>
+      </template>
+      <el-menu-item-group>
+        <el-menu-item index="2-1">发布管理</el-menu-item>
+      </el-menu-item-group>
+    </el-sub-menu>
+
+    <el-sub-menu index="3">
+      <template #title>
+        <el-icon><location /></el-icon>
+        <span>可观测性</span>
+      </template>
+      <el-menu-item-group>
+        <el-menu-item index="3-1">应用概览</el-menu-item>
+      </el-menu-item-group>
+    </el-sub-menu>
+
+    <el-sub-menu index="4">
+      <template #title>
+        <el-icon><location /></el-icon>
+        <span>监控告警</span>
+      </template>
+      <el-menu-item-group>
+        <el-menu-item index="4-1">监控查询</el-menu-item>
+        <el-menu-item index="4-2">监控统计</el-menu-item>
+        <el-menu-item index="4-3">监控描述</el-menu-item>
+        <el-menu-item index="4-4">告警记录</el-menu-item>
+        <el-menu-item index="4-5">告警配置</el-menu-item>
+        <el-menu-item index="4-6">告警角色</el-menu-item>
+        <el-menu-item index="4-7">系统监控</el-menu-item>
+        <el-menu-item index="4-8">应用监控</el-menu-item>
+      </el-menu-item-group>
+    </el-sub-menu>
   </el-menu>
 </template>
 
@@ -48,7 +65,7 @@ import {
   Setting,
 } from "@element-plus/icons-vue";
 
-const isCollapse = ref(true);
+const isCollapse = ref(false);
 const handleOpen = (key: string, keyPath: string[]) => {
   console.log(key, keyPath);
 };
