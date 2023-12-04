825f9b70a5376a4a7bac00bfda1ae24163374768
zhousujuan <179997445@qq.com>
2023-12-04 16:58:08 +0800

    菜单管理

--- src/components/layouts/BaseSide.vue
+++ src/components/layouts/BaseSide.vue
@@ -53,6 +53,72 @@
         <el-menu-item index="4-8">应用监控</el-menu-item>
       </el-menu-item-group>
     </el-sub-menu>
+
+    <el-sub-menu index="5">
+      <template #title>
+        <el-icon><location /></el-icon>
+        <span>日志账单</span>
+      </template>
+      <el-menu-item-group>
+        <el-menu-item index="5-1">日志查询</el-menu-item>
+        <el-menu-item index="5-2">账单查询</el-menu-item>
+      </el-menu-item-group>
+    </el-sub-menu>
+
+    <el-sub-menu index="6">
+      <template #title>
+        <el-icon><location /></el-icon>
+        <span>资源管理</span>
+      </template>
+      <el-menu-item-group>
+        <el-menu-item index="6-1">资源池</el-menu-item>
+        <el-menu-item index="6-2">资源使用情况</el-menu-item>
+        <el-menu-item index="6-3">资源申请审批</el-menu-item>
+        <el-menu-item index="6-4">持久卷管理</el-menu-item>
+        <el-menu-item index="6-5">K8S集群管理</el-menu-item>
+        <el-menu-item index="6-6">资源池管理</el-menu-item>
+      </el-menu-item-group>
+    </el-sub-menu>
+    <el-sub-menu index="7">
+      <template #title>
+        <el-icon><location /></el-icon>
+        <span>数据资源</span>
+      </template>
+      <el-menu-item-group>
+        <el-menu-item index="7-1">数据源</el-menu-item>
+        <el-menu-item index="7-2">路由管理</el-menu-item>
+      </el-menu-item-group>
+    </el-sub-menu>
+
+    <el-sub-menu index="8">
+      <template #title>
+        <el-icon><location /></el-icon>
+        <span>运营工具</span>
+      </template>
+      <el-menu-item-group>
+        <el-menu-item index="8-1">运营工具</el-menu-item>
+      </el-menu-item-group>
+    </el-sub-menu>
+
+    <el-sub-menu index="9">
+      <template #title>
+        <el-icon><location /></el-icon>
+        <span>权限管理</span>
+      </template>
+      <el-menu-item-group>
+        <el-menu-item index="9-1">权限管理</el-menu-item>
+      </el-menu-item-group>
+    </el-sub-menu>
+
+    <el-sub-menu index="10">
+      <template #title>
+        <el-icon><location /></el-icon>
+        <span>帮助</span>
+      </template>
+      <el-menu-item-group>
+        <el-menu-item index="10-1">开发文档</el-menu-item>
+      </el-menu-item-group>
+    </el-sub-menu>
   </el-menu>
 </template>
 
